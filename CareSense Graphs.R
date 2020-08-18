aaf9097a47b8a4d6478ade89412e55f55929a22e
kregan1 <63623055+kregan1@users.noreply.github.com>
2020-08-18 16:36:14 -0600

    Add files via upload

---
+++ CareSense Graphs.R
@@ -0,0 +1,305 @@
+###This set of graphs will look at the percent of patients that rated each clinic as excellent
+###for every month plotted against the attendance at the clinic. This set of graphs can be used
+###to determine if there is a relationship between the attendane and how well the clinic was rated 
+
+###Set working directory
+setwd("/Users/kerryregan/Desktop/CareSense Data")
+getwd()
+
+###Read in libraries
+library(readr)
+library(ggplot2)
+
+###Read in csv file for data
+SSatis<-read.csv("Sandy2019Satisfaction - Sheet1-2.csv")
+
+###Check to see if it read in properly
+SSatis
+
+###plot the data using the basic plot function. Colors can be changed at col = "blue". This code also creates
+###a legend so you can tell the difference between the two lines. Make sure that if the director is color
+###blind that you change the colors.
+plot(SSatis$Month, SSatis$Percent.Excellent, ylab = "Percent Excellent",
+     main = "Sandy Overall Satisfaction", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(SSatis$Month, SSatis$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("topright", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+
+###This plot looks at if satisfaction and the  patient visit numbers correlate
+plot(SSatis$Percent.Excellent, SSatis$Attendance, ylab = "Patient Visits", xlab = "Overall Satisfaction", 
+     main = " Sandy Satisfaction vs. Attendance")
+
+###abline draws a line that fits the linear model for the plot above
+abline(lm(SSatis$Attendance~SSatis$Percent.Excellent))
+
+###This allows you to see the results from the linear model
+Sandy<-lm(SSatis$Attendance~SSatis$Percent.Excellent)
+summary(Sandy)
+
+
+###Runs same code as above for South Jordan Clinic
+SJSatis<-read.csv("SouthJordan2019Satisfaction - Sheet1.csv")
+
+plot(SJSatis$Month, SJSatis$Percent.Excellent, ylab = "Percent Excellent",
+     main = "South Jordan Overall Satisfaction", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(SJSatis$Month, SJSatis$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("bottomleft", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+plot(SJSatis$Percent.Excellent, SJSatis$Attendance, ylab = "Attendance", xlab = "Overall Satisfaction", 
+     main = " South Jordan Attendance vs. Satisfaction")
+
+abline(lm(SJSatis$Attendance~SJSatis$Percent.Excellent))
+
+
+###Runs same code as above for West Jordan clinic
+WJSatis<-read.csv("WestJordan2019Satisfaction - Sheet1-2.csv")
+
+plot(WJSatis$Month, WJSatis$Percent.Excellent, ylab = "Percent Excellent",
+     main = "West Jordan Overall Satisfaction", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(WJSatis$Month, WJSatis$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("topright", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+plot(WJSatis$Percent.Excellent, WJSatis$Attendance, ylab = "Attendance", xlab = "Overall Satisfaction", 
+     main = " West Jordan Attendance vs. Satisfaction")
+
+abline(lm(WJSatis$Attendance~WJSatis$Percent.Excellent))
+
+plot(WJSatis$Month, WJSatis$Percent.Excellent, lty=1)
+
+
+attach(WJSatis)
+plot(as.vector(Month[1:12]), Percent.Excellent[1:12], pch=1)
+
+Month[2]
+
+
+CWHSatis<-read.csv("CWH2019Satisfaction - Sheet1.csv")
+
+plot(CWHSatis$Month, CWHSatis$Percent.Excellent, ylab = "Percent Excellent",
+     main = "CWH Overall Satisfaction", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(CWHSatis$Month, CWHSatis$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("topright", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+plot(CWHSatis$Percent.Excellent, CWHSatis$Attendance, ylab = "Attendance", xlab = "Overall Satisfaction", 
+     main = " West Jordan Attendance vs. Satisfaction")
+
+abline(lm(CWHSatis$Attendance~CWHSatis$Percent.Excellent))
+
+plot(CWHSatis$Month, CWHSatis$Percent.Excellent, lty=1)
+
+
+
+DraperSatis<-read.csv("Draper2019Satisfaction - Sheet1-2.csv")
+
+plot(DraperSatis$Month, DraperSatis$Percent.Excellent, ylab = "Percent Excellent",
+     main = "Draper Overall Satisfaction", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(DraperSatis$Month, DraperSatis$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("topright", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+
+RivertonSatis<-read.csv("Riverton2019Satisfaction - Sheet1-3.csv")
+
+plot(RivertonSatis$Month, RivertonSatis$Percent.Excellent, ylab = "Percent Excellent",
+     main = "Riverton Overall Satisfaction", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(RivertonSatis$Month, RivertonSatis$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("topright", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+
+MurraySatis<-read.csv("Murray2019Satisfaction - Sheet1-2.csv")
+
+plot(MurraySatis$Month, MurraySatis$Percent.Excellent, ylab = "Percent Excellent",
+     main = "Murray Overall Satisfaction", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(MurraySatis$Month, MurraySatis$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("topright", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+
+SSSatis<-read.csv("SS2019Satisfaction - Sheet1-2.csv")
+
+plot(SSSatis$Month, SSSatis$Percent.Excellent, ylab = "Percent Excellent",
+     main = "SS Overall Satisfaction", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(SSSatis$Month, SSSatis$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("topright", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+SBSatis<-read.csv("SB2019Satisfaction - Sheet1.csv")
+
+plot(SBSatis$Month, SBSatis$Percent.Excellent, ylab = "Percent Excellent",
+     main = "SB Overall Satisfaction", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(SBSatis$Month, SBSatis$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("topright", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+
+###This plots percent rated excellent for all clinics for each month. We did this to provide some
+###information for the directors in case they suspect something was happening during those months. 
+###We also wanted to see if all clinics showed any trends. 
+
+AllPE=read.csv("PercentExcellent2019All - Sheet1.csv")
+plot(subset(AllPE$Month, AllPE$Clinic=="CWH"), subset(AllPE$Percent.Excellent, AllPE$Clinic=="CWH"), col="red", type="line", lty="dotted", lwd=4, ylim=c(70,100), main = "Percent Excellent Overall Experience 2019", ylab = "Percent Excellent", xlab = "Month")
+lines(subset(AllPE$Month, AllPE$Clinic=="Draper"), subset(AllPE$Percent.Excellent, AllPE$Clinic=="Draper"), col="blue", type="line", lwd=4, lty="solid")
+lines(subset(AllPE$Month, AllPE$Clinic=="Murray"), subset(AllPE$Percent.Excellent, AllPE$Clinic=="Murray"), col="yellow", type="line", lwd=4, lty="solid")
+lines(subset(AllPE$Month, AllPE$Clinic=="Sandy"), subset(AllPE$Percent.Excellent, AllPE$Clinic=="Sandy"), col="green", type="line", lwd=4, lty="solid")
+lines(subset(AllPE$Month, AllPE$Clinic=="SS"), subset(AllPE$Percent.Excellent, AllPE$Clinic=="SS"), col=261, type="line", lwd=4, lty="solid")
+lines(subset(AllPE$Month, AllPE$Clinic=="SB"), subset(AllPE$Percent.Excellent, AllPE$Clinic=="SB"), col="black", type="line", lwd=4, lty="solid")
+lines(subset(AllPE$Month, AllPE$Clinic=="SJ"), subset(AllPE$Percent.Excellent, AllPE$Clinic=="SJ"), col=526, type="line", lwd=4, lty="solid")
+lines(subset(AllPE$Month, AllPE$Clinic=="WJ"), subset(AllPE$Percent.Excellent, AllPE$Clinic=="WJ"), col=152, type="line", lwd=4, lty="solid")
+lines(subset(AllPE$Month, AllPE$Clinic=="Riverton"), subset(AllPE$Percent.Excellent, AllPE$Clinic=="Riverton"), col="purple", type="line", lwd=4, lty="solid")
+legend(1,88, legend = c("CWH", "Draper", "Murray", "Riverton", "Sandy", "SS", "SB", "SJ", "WJ"), col=c("red", "blue", "yellow", "purple", "green", 261, "black", 526, 152), lty = c("dotted", "solid", "solid", "solid", "solid", "solid", "solid", "solid", "solid"), lwd=4)
+
+
+
+
+
+
+CWHKnow<-read.csv("CWH2019Knowledge - Sheet1.csv")
+
+plot(CWHKnow$Month, CWHKnow$Percent.Excellent, ylab = "Percent Excellent",
+     main = "CWH Knowledge of the Therapist", xlab = "Month", col = "blue", type = "l", lty="dotted")
+par(new = TRUE)
+plot(CWHKnow$Month, CWHKnow$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("bottomleft", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(2, 1))
+
+
+DraperKnow<-read.csv("Draper2019Knowledge - Sheet1.csv")
+
+plot(DraperKnow$Month, DraperKnow$Percent.Excellent, ylab = "Percent Excellent",
+     main = "Draper Knowledge of the Therapist", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(DraperKnow$Month, DraperKnow$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("bottomleft", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+MurrayKnow<-read.csv("Murray2019Knowledge - Sheet1.csv")
+
+plot(MurrayKnow$Month, MurrayKnow$Percent.Excellent, ylab = "Percent Excellent",
+     main = "Murray Knowledge of the Therapist", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(MurrayKnow$Month, MurrayKnow$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("topleft", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+RivertonKnow<-read.csv("Riverton2019Knowledge - Sheet1.csv")
+
+plot(RivertonKnow$Month, RivertonKnow$Percent.Excellent, ylab = "Percent Excellent",
+     main = "Riverton Knowledge of the Therapist", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(RivertonKnow$Month, RivertonKnow$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("bottomleft", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+SandyKnow<-read.csv("Sandy2019Knowledge - Sheet1.csv")
+
+plot(SandyKnow$Month, SandyKnow$Percent.Excellent, ylab = "Percent Excellent",
+     main = "Sandy Knowledge of the Therapist", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(SandyKnow$Month, SandyKnow$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("bottomleft", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+SSKnow<-read.csv("SS2019Knowledge - Sheet1.csv")
+
+plot(SSKnow$Month, SSKnow$Percent.Excellent, ylab = "Percent Excellent",
+     main = "SS Knowledge of the Therapist", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(SSKnow$Month, SSKnow$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("topleft", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+SBKnow<-read.csv("SB2019Knowledge - Sheet1.csv")
+
+plot(SBKnow$Month, SBKnow$Percent.Excellent, ylab = "Percent Excellent",
+     main = "SB Knowledge of the Therapist", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(SBKnow$Month, SBKnow$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("bottomleft", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+SJKnow<-read.csv("SJ2019Knowledge - Sheet1.csv")
+
+plot(SJKnow$Month, SJKnow$Percent.Excellent, ylab = "Percent Excellent",
+     main = "SJ Knowledge of the Therapist", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(SJKnow$Month, SJKnow$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("bottomleft", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
+WJKnow<-read.csv("WJ2019Knowledge - Sheet1.csv")
+
+plot(WJKnow$Month, WJKnow$Percent.Excellent, ylab = "Percent Excellent",
+     main = "WJ Knowledge of the Therapist", xlab = "Month", col = "blue", type = "l")
+par(new = TRUE)
+plot(WJKnow$Month, WJKnow$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1, type = "l")
+axis(side = 4)
+mtext("", side = 4, line = 3)
+legend("bottomleft", c("Percent Excellent", "Attendance"),
+       col = c("blue", "red"), lty = c(1, 1))
+
---
+++ CareSenseDataset.R
@@ -0,0 +1,152 @@
+##Working Directory
+getwd()
+setwd("/Users/kerryregan/Desktop/CareSense Data")
+library(readr)
+
+
+
+WJSatis<-read.csv("WestJordan2019Satisfaction - Sheet1.csv")
+
+plot(WJSatis$Month, WJSatis$Percent.Excellent, ylab = "Percent Excellent",
+     main = "West Jordan Overall Satisfaction", xlab = "Month", col = "blue")
+par(new = TRUE)
+plot(WJSatis$Month, WJSatis$Attendance, xaxt = "n", yaxt = "n",
+     ylab = "", xlab = "", col = "red", lty = 1)
+axis(side = 4)
+mtext("", side = 4, line = 3)
+
+
+
+
+legend("topleft", c("beaver1", "beaver2"),
+       col = c("blue", "red"), lty = c(1, 2))
+
+
+
+
+LEFSv1<-read.vsv("LEFS1vDiff - Sheet1.csv")
+
+LEFS<-read.csv("ClinicLEFSDiff.csv", col_types(Clinic=col_character(), Visits=col_number(), Difference=col_number())
+
+plot(subset(LEFS$Visits,LEFS$Clinic=="Sandy"), subset(LEFS$Difference,LEFS$Clinic=="Sandy"), ylim = c(-10,50), xlim = c(5,50), ylab = "Difference", xlab = "Visits"), main = "Difference in LEFS vs. Visit Number")
+
+
+PhoneVAttendance<-read.csv("AttendancevsPhone.csv", col_types=cols(Clinic=col_character(), Attendance=col_number(), Phone=col_number()))
+
+KnowledgeVSAttendance<-read_csv("AttendanceVsKnowledge.csv", col_types=cols(Clinic=col_character(), Attendance=col_number(), Knowledge=col_number()))
+
+##Read in Knowledge of PT Data
+KnowledgeOfPT<-read_csv("KnowledgeOfPTJanFeb - Sheet1.csv", col_types=cols(Clinic=col_character(), Rate=col_number()))
+View(KnowledgeOfPT)
+
+##Read in Knowledge of PT Data
+PatientNum<-read_csv("PatientNumJanFebMean3 - Sheet1.csv", col_types=cols(Clinic=col_character(), Total=col_number(), WeeklyTherapist=col_number(), Mean=col_number()))
+View(PatientNum)
+
+PatientSatisBefore<-read_csv("PatientSatisfactionBefore - Sheet1.csv", col_types=cols(Clinic=col_character(), Rate=col_number()))
+
+PatientSatisAfter<-read_csv("PatientSatisfactionAfter - Sheet1.csv", col_types=cols(Clinic=col_character(), Rate=col_number()))
+
+##Read in Overall Experience##
+OverallExperience<-read_csv("OverallExperience - Sheet1.csv", col_types=cols(Clinic=col_character(), Rate=col_number()))
+View(OverallExperience)
+
+##Read in Getting in by phone##
+Phone<-read_csv("GettingByPhoneJanFeb - Sheet1.csv", col_types=cols(Clinic=col_character(), Rate=col_number()))
+View(Phone)
+
+##Read in Percentage vs Patient Satisfaction##
+Attend<-read_csv("PercentagevsPatientSatisfaction - Sheet1.csv", col_types=cols(Clinic=col_character(), Rate=col_number()))
+View(Phone)
+
+
+##Set Variable
+DataFile<-KnowledgeOfPT
+DataFile<-OverallExperience
+DataFile<-Phone
+DataFile<-PatientSatisAfter
+
+##Run ANOVA##
+Init<-aov(DataFile$Rate~DataFile$Clinic)
+Init
+summary(Init)
+
+##Get mean for each clinic##
+CottonwoodMean<-mean(subset(DataFile$Rate, DataFile$Clinic=="CWH"))
+DraperMean<-mean(subset(DataFile$Rate, DataFile$Clinic=="Draper"))
+MurrayMean<-mean(subset(DataFile$Rate, DataFile$Clinic=="Murray"))
+RivertonMean<-mean(subset(DataFile$Rate, DataFile$Clinic=="Riverton"))
+SandyMean<-mean(subset(DataFile$Rate, DataFile$Clinic=="Sandy"))
+SaratogaMean<-mean(subset(DataFile$Rate, DataFile$Clinic=="Saratoga"))
+SBangeterMean<-mean(subset(DataFile$Rate, DataFile$Clinic=="SB"))
+SouthJMean<-mean(subset(DataFile$Rate, DataFile$Clinic=="SJ"))
+WestJMean<-mean(subset(DataFile$Rate, DataFile$Clinic=="WJ"))
+
+##Data Vectors##
+
+AllMeans<-c(CottonwoodMean, DraperMean, MurrayMean, RivertonMean, SandyMean, SaratogaMean, SouthJMean, WestJMean)
+AllErrors<-c(CottonwoodError, DraperError, MurrayError, RivertonError, SandyError, SaratogaError, SouthJError, WestJError)
+AllClinics<-c("CHW", "Draper", "Murray", "Riverton", "Sandy", "Saratoga", "SJ", "WJ")
+
+##Get standard error for each clinic##
+CottonwoodError<-sd(subset(DataFile$Rate, DataFile$Clinic=="CWH"))/sqrt(length(DataFile$Clinic=="CHW"))
+DraperError<-sd(subset(DataFile$Rate, DataFile$Clinic=="Draper"))/sqrt(length(DataFile$Clinic=="Draper"))
+MurrayError<-sd(subset(DataFile$Rate, DataFile$Clinic=="Murray"))/sqrt(length(DataFile$Clinic=="Murray"))
+RivertonError<-sd(subset(DataFile$Rate, DataFile$Clinic=="Riverton"))/sqrt(length(DataFile$Clinic=="Riverton"))
+SandyError<-sd(subset(DataFile$Rate, DataFile$Clinic=="Sandy"))/sqrt(length(DataFile$Clinic=="Sandy"))
+SaratogaError<-sd(subset(DataFile$Rate, DataFile$Clinic=="Saratoga"))/sqrt(length(DataFile$Clinic=="Saratoga"))
+SBangeterError<-sd(subset(DataFile$Rate, DataFile$Clinic=="SB"))/sqrt(length(DataFile$Clinic=="SB"))
+SouthJError<-sd(subset(DataFile$Rate, DataFile$Clinic=="SJ"))/sqrt(length(DataFile$Clinic=="SJ"))
+WestJError<-sd(subset(DataFile$Rate, DataFile$Clinic=="WJ"))/sqrt(length(DataFile$Clinic=="WJ"))
+
+##Make a plot##
+
+means <- c(CottonwoodMean, DraperMean, MurrayMean, RivertonMean, SandyMean, SaratogaMean, SBangeterMean, SouthJMean, WestJMean)
+names <- c("CWH", "Draper", "Murray", "Riverton", "Sandy", "Saratoga","SB", "SJ", "WJ")
+standardErrors <- c(CottonwoodError, DraperError, MurrayError, RivertonError, SandyError, SaratogaError, SBangeterError, SouthJError, WestJError)
+plotTop <- max(means+standardErrors*2)
+barCenters <- barplot(means, names.arg=names, col="gray", las=2, ylim=c(4.5, 5), main="Please Rate Ability to Get in By Phone")
+segments(barCenters, means-standardErrors*2, barCenters, means+standardErrors*2, lwd=2)
+
+
+means <- c(CottonwoodMean, DraperMean, MurrayMean, RivertonMean, SandyMean, SaratogaMean, SBangeterMean, SouthJMean, WestJMean)
+names <- c("CWH", "Draper", "Murray", "Riverton", "Sandy", "Saratoga","SB", "SJ", "WJ")
+standardErrors <- c(CottonwoodError, DraperError, MurrayError, RivertonError, SandyError, SaratogaError, SBangeterError, SouthJError, WestJError)
+plotTop <- max(means+standardErrors*2)
+barCenters <- barplot(means, names.arg=names, col="gray", las=2, ylim=c(4.5, 5), main="Please Rate Ability to Get in By Phone")
+segments(barCenters, means-standardErrors*2, barCenters, means+standardErrors*2, lwd=2)
+
+
+
+
+means <- c(CottonwoodMean, RivertonMean, SandyMean)
+names <- c("CWH", "Riverton", "Sandy")
+standardErrors <- c(CottonwoodError, RivertonError, SandyError) 
+plotTop <- max(means+standardErrors*2)
+barCenters <- barplot(means, names.arg=names, col="gray", las=2, ylim=c(3.7, 4), main="Patient Satisfaction: Jan-March")
+segments(barCenters, means-standardErrors*2, barCenters, means+standardErrors*2, lwd=2)
+
+
+
+
+
+
+
+##Read in Regression Data##
+
+RegressionPTvOE<-read_csv("KnowPTvsOverallEx - Sheet1.csv", col_types=cols(Clinic=col_character(), Rate=col_number()))
+
+RegressionKNOWvATT<-aov(KnowledgeVSAttendance)
+
+RegressionAVP<-aov(PhoneVAttendance)
+
+
+##Read in Dataset 1##
+OverallExperience<-read_csv("OverallExperience5Weeks - Sheet1.csv", col_types=cols(Clinic=col_character()))
+View(OverallExperience)
+
+
+##Read in Dataset 2##
+Patients<-read_csv("NumberofPatients5Weeks - Sheet1.csv", col_types=cols(Clinic=col_character()))
+View(Patients)
+
